db56b53478fda60efdea5610e0f2f48097512e3e
nsushkov <Nikita.Sushkov@comindware.com>
2016-11-11 17:12:27 +0300

    main form changes

--- app/actions/mainFormActions.js
+++ app/actions/mainFormActions.js
@@ -1,4 +1,7 @@
 import {createAction} from 'redux-actions'
 
 export const SUBMIT_MAIN_FORM = 'SUBMIT_MAIN_FORM'
+export const UNLOCK_MAIN_FORM = 'UNLOCK_MAIN_FORM'
+
 export const submitMainForm = createAction(SUBMIT_MAIN_FORM, (data, options) => ({data, options}))
+export const unlockMainForm = createAction(UNLOCK_MAIN_FORM)
--- app/components/mainForm/mainForm.jsx
+++ app/components/mainForm/mainForm.jsx
@@ -8,7 +8,7 @@ import CSSModules from 'react-css-modules'
 import styles from './mainForm.module.css'
 import BasicInput from '../fields/basicInput.jsx'
 import TextArea from '../fields/textArea.jsx'
-import {submitMainForm} from '../../actions/submitFormActions'
+import {submitMainForm, unlockMainForm} from '../../actions/submitFormActions'
 import locHelper from '../../helpers/locHelper'
 
 const mapProps = {
@@ -20,10 +20,12 @@ const modelPath = 'formState.mainForm'
 
 export const MainForm = setPropTypes({
     intl: intlShape.isRequired,
+    handleUnlock: React.PropTypes.func.isRequired,
     handleSubmit: React.PropTypes.func.isRequired
 })(CSSModules(props => {
     const {
         formState,
+        handleUnlock,
         handleSubmit,
         intl: {
             messages
@@ -35,11 +37,15 @@ export const MainForm = setPropTypes({
     return (
         <Form model={modelPath} className="container" onSubmit={handleSubmit}>
             <fieldset styleName="fields">
-                <legend styleName="title"></legend>
+                <legend styleName="title">{getLoc('title')}</legend>
                 <div class="row">
-                    <Control type="text" model=".pin" placeholder="Pin" messages={messages}
-                             mapProps={mapProps}
-                             component={BasicInput}/>
+                    <div styleName="col">
+                        <Control type="text" model=".pin" placeholder="Pin" messages={messages} mapProps={mapProps}
+                                 component={BasicInput}/>
+                    </div>
+                    <div className="col">
+                        <button className="btn waves-effect waves-light" title={getLoc('unlock')} onClick={handleUnlock} type="button">{getLoc('unlock')}</button>
+                    </div>
                 </div>
             </fieldset>
         </Form>
@@ -50,5 +56,6 @@ export default injectIntl(connect((state, ownProps) => ({
     formState: getModel(state, formPath),
     intl: ownProps.intl
 }), (dispatch) => ({
+    handleUnlock: (pin) => dispatch(unlockMainForm(pin)),
     handleSubmit: data => dispatch(submitMainForm(data))
 }))(MainForm))
