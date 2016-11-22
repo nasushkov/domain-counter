72573f898c9b0103e50f674fe52cc3746f4d2409
nsushkov <Nikita.Sushkov@comindware.com>
2016-11-22 17:35:49 +0300

    file input fixes

--- app/components/fields/fileInput.jsx
+++ app/components/fields/fileInput.jsx
@@ -1,13 +1,29 @@
 import React, {Component} from 'react'
 import classNames from 'classnames'
 import _ from 'lodash'
-import {setPropTypes, pure} from 'recompose'
+import {setPropTypes, pure, withHandlers, compose} from 'recompose'
 
-export default pure(setPropTypes({
-    innerState: React.PropTypes.object.isRequired
-})((props) => {
+const enhance = compose(
+    setPropTypes({
+        innerState: React.PropTypes.object.isRequired
+    }),
+    withHandlers({
+        onFileChange: (props) => {
+            return (event) => {
+                return props.onChange(event)
+            }
+        },
+        onFileClick:  (props) => {
+            return (event) => {
+                return props.onChange(null)
+            }
+        },
+    })
+)
+
+export default pure(enhance((props) => {
     const inputValue =  (props.innerState.value && props.innerState.value.length)
-        ? props.innerState.value.reduce((prevVal, curVal) => prevVal.concat(curVal, ', '), [])
+        ? props.innerState.value.reduce((prevVal, curVal) => prevVal.concat(curVal.name, ', '), [])
         : []
 
     if(inputValue.length) {
@@ -22,16 +38,16 @@ export default pure(setPropTypes({
         [props.textClassName]: props.textClassName
     })
 
-    const inputProps = _.omit(props, ['placeholder', 'innerState', 'className', 'messages', 'buttonText'])
+    const inputProps = _.omit(props, ['placeholder', 'innerState', 'textClassName', 'messages', 'buttonText', 'onChange', 'onFileChange', 'onFileClick'])
 
     return (
         <div className="file-field input-field">
             <div className={btnClassName}>
                 <span>{props.buttonText}</span>
-                <input {...inputProps} type="file"/>
+                <input {...inputProps} onChange={props.onFileChange} onClick={props.onFileClick} type="file"/>
             </div>
             <div className="file-path-wrapper">
-                <input disabled={props.disabled} className={textInputClassName} type="text" placeholder={props.placeholder}/>
+                <input disabled={props.disabled} value={inputValue.join('')} className={textInputClassName} type="text" placeholder={props.placeholder}/>
             </div>
         </div>
     );
